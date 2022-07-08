d234c143e6e22d1009195de3e2bf59d31442dded
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2022-07-07 23:37:54 -0500

    added a keybinding (mod shift tab) for switching to a random wallpaper found in wallpapers.py; there is a bug where the keybinding only works if you reload the config the run the keybinding

--- config.py
+++ config.py
@@ -31,10 +31,12 @@ from libqtile.utils import guess_terminal
 
 from os.path import expanduser
 from subprocess import Popen
+from random import randint
 
 from dracula import dracula
 from catpuccin import catpuccin
 from widgets import widgets
+from wallpapers import wallpapers
 
 mod = "mod4"
 terminal = guess_terminal()
@@ -87,6 +89,13 @@ keys = keys = [
     # Change the volume if your keyboard has special volume keys.
     Key([], "XF86AudioRaiseVolume",lazy.spawn("amixer -D pulse sset Master 5%+")),
     Key([], "XF86AudioLowerVolume",lazy.spawn("amixer -D pulse sset Master 5%-")),
+    Key(
+        [mod, "shift"],
+        "Tab",
+        lazy.spawn(
+            f"nitrogen --set-scaled {wallpapers[randint(1,5)]}"
+        )
+    )
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -117,8 +126,8 @@ for i in groups:
 
 
 default_layout_args = dict(
-    border_focus=catpuccin["lavender"],
-    border_normal=catpuccin["gray"],
+    border_focus=catpuccin["darklavender"],
+    border_normal=dracula["black"],
     border_width=4,
 )
 
